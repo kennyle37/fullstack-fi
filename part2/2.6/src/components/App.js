8784b5b1d8b58a70f3e5f515199a133e645e49d8
Kenny Le <kennyle37@gmail.com>
2019-10-10 13:11:13 -0700

    2.11

--- part2/2.6/src/components/App.js
+++ part2/2.6/src/components/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
 import Numbers from './Persons';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons] = useState([])
+  //   { name: 'Arto Hellas', number: '040-123456' },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523' },
+  //   { name: 'Dan Abramov', number: '12-43-234345' },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  // ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('');
   const [ newFilter, setNewFilter ] = useState('');
@@ -18,6 +20,17 @@ const App = () => {
   const handleOnNumberChange = (e) => setNewNumber(e.target.value);
   const handleFilter = (e) => setNewFilter(e.target.value);
   const verifyDuplicate = (name) => persons.every(person => person.name !== name);
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('response', response.data);
+        setPersons(response.data);
+      })
+  }
+
+  useEffect(hook, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- part2/2.6/src/components/Persons.js
+++ part2/2.6/src/components/Persons.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Numbers = ({ persons, filter }) => {
   const handleDisplayPerson = persons.map(person => {
-    if (!filter) return <div key={person.name}>{person.name} {person.number}</div>
     const lowerCaseName  = person.name.toLowerCase();
     const lowerCaseFilter = filter.toLowerCase();
 
-    if (lowerCaseName.includes(lowerCaseFilter)) {
+    if (!filter) {
       return <div key={person.name}>{person.name} {person.number}</div>
-    }
+    } else if (lowerCaseName.includes(lowerCaseFilter)) {
+        return <div key={person.name}>{person.name} {person.number}</div>
+    } 
+      return true;
   })
 
   return (
