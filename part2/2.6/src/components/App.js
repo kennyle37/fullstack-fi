4a867a9d73e027cc4ffb7f488ff810532255f986
Kenny Le <kennyle37@gmail.com>
2019-10-11 12:24:27 -0700

    2.16

--- part2/2.6/src/components/App.js
+++ part2/2.6/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-
+import phoneBookservice from '../services/phonebook'
 // import Numbers from './Persons';
 // import Filter from './Filter';
 // import PersonForm from './PersonForm';
 import Phonebook from './Phonebook';
 import Countries from './Countries';
+
 
 const App = () => {
   const [ persons, setPersons] = useState([])
@@ -20,17 +21,12 @@ const App = () => {
   const handleFilter = (e) => setNewFilter(e.target.value);
   const verifyDuplicate = (name) => persons.every(person => person.name !== name);
 
-  const getPhonebookQuery = () => {
-    axios
-      .get('http://localhost:3001/phonebook')
-      .then(res => {
-        console.log(res.data)
-        setPersons(res.data)
-      })
+  useEffect(() => {
+    phoneBookservice
+      .getAll()
+      .then(res => setPersons(res))
       .catch(err => console.log(err))
-  }
-
-  useEffect(getPhonebookQuery, []);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -40,21 +36,19 @@ const App = () => {
       number: newNumber
     }
     if (verify) {
-      axios
-        .post('http://localhost:3001/phonebook', newPerson)
+      phoneBookservice
+        .createItem(newPerson)
         .then(res => {
-          setPersons(persons.concat(res.data))
+          setPersons(persons.concat(res))
           setNewName('')
           setNewNumber('')
         })
-        .catch(error => console.log(error))
     } else {
       return alert(`${newName} is already in the phone book`)
     }
   }
 
   const handleCountry = (e) => setSearchCountry(e.target.value);
-
 
   const countriesQuery = () => {
     if (!searchCountry) return;
---
+++ part2/2.6/src/services/phonebook.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/phonebook';
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return (
+    request
+      .then(res => res.data)
+      .catch(err => console.log(err))
+  )
+}
+const createItem = ( payload ) => {
+  const request = axios.post(baseUrl, payload)
+  return (
+    request
+      .then(res => res.data)
+      .catch(err => console.log(err))
+  )
+}
+
+const updateItem = ( id, payload ) => axios.patch(`${baseUrl}/${id}`, payload)
+
+const deleteItem = ( id ) => axios.delete(`${baseUrl}/${id}`) 
+
+export default {
+  getAll,
+  createItem,
+  updateItem,
+  deleteItem
+}
