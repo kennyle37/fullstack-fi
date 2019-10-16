152ea9b6818822b1305e411ffe7599cc38bf9e79
Kenny Le <kennyle37@gmail.com>
2019-10-16 10:29:13 -0700

    3.5

--- part3/3/index.js
+++ part3/3/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-notes = [
+persons = [
   {
     name: "Arto Hellas",
     number: "123-456",
@@ -24,35 +25,62 @@ notes = [
   }
 ]
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('<h1>hello world</h1>')
 })
 
 app.get('/persons', (req, res) => {
-  res.json(notes);
+  res.json(persons);
 })
 
 app.get('/info/', (req, res) => {
-  const n = notes.length;
+  const n = persons.length;
   const date = Date();
   res.send(`Phonebook has info for ${n} people. <br><br>${date}`)
 })
 
 app.get('/persons/:id', (req, res) => {
   const id = Number(req.params.id);
-  const note = notes.find(note => note.id === id);
+  const person = persons.find(person => person.id === id);
 
-  if (note) {
-    res.send(note)
+  if (person) {
+    res.send(person)
   } else {
-    res.send(404)
+    res.status(404).send('Retrieved unsucessful')
   }
+})
+
+/*
+  generate our id to add
+  concat it to persons
+  send the persons
+*/
+app.post('/persons', (req, res) => {
+  const body = req.body;
+  const id = Math.floor(Math.random() * 1000) + 1 
+  console.log(body)
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number
+  }
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'missing content'
+    })
+  }
+  
+  persons = persons.concat(person);
+  res.json(persons);
 })
 
 app.delete('/persons/:id', (req, res) => {
   const id = Number(req.params.id);
-  notes = notes.filter(note => note.id !== id);
-  res.status(204).end();
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).json('User deleted');
 })
 
 const PORT = 3001;
