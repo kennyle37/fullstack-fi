1bc0d3b452f792e0d256efcd20083d05a0a7b510
Kenny Le <kennyle37@gmail.com>
2019-10-15 20:25:18 -0700

    3.4

--- part3/3/index.js
+++ part3/3/index.js
@@ -32,10 +32,27 @@ app.get('/persons', (req, res) => {
   res.json(notes);
 })
 
-app.get('/info', (req, res) => {
+app.get('/info/', (req, res) => {
   const n = notes.length;
   const date = Date();
   res.send(`Phonebook has info for ${n} people. <br><br>${date}`)
+})
+
+app.get('/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find(note => note.id === id);
+
+  if (note) {
+    res.send(note)
+  } else {
+    res.send(404)
+  }
+})
+
+app.delete('/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+  res.status(204).end();
 })
 
 const PORT = 3001;
