6ce4cd46dd034d569262792af971f771a2a7a840
Kenny Le <kennyle37@gmail.com>
2019-10-17 13:20:02 -0700

    package

--- part3/Lesson/index.js
+++ part3/Lesson/index.js
@@ -13,7 +13,8 @@ app.use(cors());
 app.use('/phonebook', phonebookRoutes)
 app.use('', notesRoutes)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`server is running on ${PORT}`)
 })
