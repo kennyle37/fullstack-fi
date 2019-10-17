a8cbd6d16e13ac40fa276019427842d2c6d815ee
Kenny Le <kennyle37@gmail.com>
2019-10-17 12:40:38 -0700

    build for front end

---
+++ part3/lesson/build/precache-manifest.ad6f3c207a91f67e16d21fd112b218a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d748d9f48bdeae8b918a4b9f3b4941f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70e2f25c62806aa12f21",
+    "url": "/static/js/2.08b5f1e7.chunk.js"
+  },
+  {
+    "revision": "fbf959f21461a519635e",
+    "url": "/static/js/main.93ac7030.chunk.js"
+  },
+  {
+    "revision": "de71d062d2827f8e6de2",
+    "url": "/static/js/runtime-main.b96effd9.js"
+  }
+]);
