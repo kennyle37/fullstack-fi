88ec26603180bbb100c07fc71ee3aedaf9a1aaa5
Kenny Le <kennyle37@gmail.com>
2019-10-20 21:37:16 -0700

    fix front end for 3

--- part3/3/server.js
+++ part3/3/server.js
@@ -2,26 +2,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors')
 
-// const personRoutes = require('./routes/persons');
-// const notesRoutes = require('./routes/notes');
-// const phonebookRoutes = require('./routes/phonebook');
+const personRoutes = require('./routes/persons');
+const notesRoutes = require('./routes/notes');
+const phonebookRoutes = require('./routes/phonebook');
 
+app.use(cors())
 // app.use(express.static('build'))
-// app.use(bodyParser.json());
-// app.use('/phonebooks', phonebookRoutes)
-// app.use('/persons', personRoutes);
-// app.use('/notes', notesRoutes)
+app.use(bodyParser.json());
+app.use('/', phonebookRoutes)
+app.use('/persons', personRoutes);
+app.use('/notes', notesRoutes)
 
-app.get('/', (req, res) => {
-  console.log('reached')
-  res.send('hi')
-})
+morgan.token('body', (req) => JSON.stringify(req.body))
+morgan.token('status', (res) => res.statusCode)
 
-// morgan.token('body', (req) => JSON.stringify(req.body))
-// morgan.token('status', (res) => res.statusCode)
-
-// app.use(morgan(':method :url :status :response-time MS :body :status'))
+app.use(morgan(':method :url :status :response-time MS :body :status'))
 
 const PORT = process.env.SERVERPORT || 3001;
 
