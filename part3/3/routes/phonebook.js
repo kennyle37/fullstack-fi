9f49aa5265e364d5291beed4a4e32f59c94dcfe5
Kenny Le <kennyle37@gmail.com>
2019-10-21 12:12:11 -0700

    refactored backend

--- part3/3/routes/phonebook.js
+++ part3/3/routes/phonebook.js
@@ -2,19 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Phonebook = require('../models/phonebook')
 
-let phonebook = [
-  {
-    "name": "Arty",
-    "number": "123 45",
-    "id": 1
-  },
-  {
-    "name": "Kyle",
-    "number": "142",
-    "id": 2
-  }
-]
-
 router
   .get('/', (req, res) => {
     Phonebook.find({}).then(entry => {
@@ -22,62 +9,42 @@ router
     })
   })
 
-router.
-  post('/', (req, res) => {
-    Phonebook.findOne({
-      name: req.body.name
-    })
-    .then(entry => {
-      console.log(entry)
-      if (entry === null) {
-        const newEntry = new Phonebook({
-          "name": req.body.name,
-          "number": req.body.number
+router
+    .post('/', (req, res) => {
+      const newEntry = new Phonebook({
+        "name": req.body.name,
+        "number": req.body.number
+      })
+      newEntry
+        .save()
+        .then(response => {
+          mongoose.connection.close()
         })
-        newEntry
-          .save()
-          .then(response => {
-            console.log('saved', response)
-            mongoose.connection.close()
-            res.status(200).json('Created entry', response);
-          })
-          .catch(error => {
-            console.error(error)
-          })
-        } else {
-          res.json('User already exists!')
-          res.status(400);
-        }
-      })
-      .catch(err => {
-        console.error('unable to create', err)
-      })
-
-  })
+      res.status(200).json({ "content created": newEntry })
+    })
 
 router
   .delete('/:id', (req, res) => {
-    const id = Number(req.params.id);
-    phonebook = phonebook.filter(person => person.id !== id);
-    res.status(200).json(phonebook)
+    Phonebook
+      .findByIdAndDelete(req.params.id)
+      .then(response => {
+        mongoose.connection.close()
+      })
+    res.status(200).json("content deleted");
   })
 
 router
   .put('/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const updatedPhonebook = phonebook.map(person => {
-      if (person.id === id) {
-        return {
-          id: id,
-          name: req.body.name,
-          number: req.body.number
-        }
-      } else {
-        return person
-      }
-    })
-    phonebook = updatedPhonebook;
-    res.status(200).json(updatedPhonebook);
+    Phonebook
+      .findByIdAndUpdate(req.params.id, {
+        "name": req.body.name,
+        "number": req.body.number
+      })
+      .then(response => {
+        mongoose.connection.close()
+      })
+      .catch(err => console.error(err))
+    res.status(200).json("content updated!")
   })
 
 module.exports = router;
--- part3/3/src/services/phonebook.js
+++ part3/3/src/services/phonebook.js
@@ -6,8 +6,6 @@ const getAll = () => {
   return (
     request
       .then(res => {
-        // console.log('reached')
-        // console.log(res)
         return res.data
       })
       .catch(err => console.log(err))
